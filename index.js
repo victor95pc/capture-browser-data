d4eaad5fb58398168560989b274208a7a24e4602
Victor Palomo de Castro <victor.palomo@fulllab.com.br>
2018-01-29 00:50:56 -0200

    setup server with basic request to save user agent and screen size

---
+++ index.js
@@ -0,0 +1,34 @@
+const fastify = require('fastify')();
+const path    = require('path');
+const fetch   = require('node-fetch');
+
+const WEBDIS = "http://35.196.211.117";
+
+let saveCapturedData = function (capturedData) {
+  return fetch(`${WEBDIS}/LPUSH/browser-captured-data/${encodeURIComponent(capturedData)}`);
+}
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/'
+})
+
+fastify.get('/', function (request, reply) {
+  reply.sendFile('index.html');
+})
+
+fastify.post('/capture', function (request, reply) {
+  saveCapturedData(JSON.stringify(request.body)).then(function() {
+    reply.code(200);
+    reply.send();
+
+  }).catch(function() {
+    reply.code(500);
+    reply.send();
+  });
+})
+
+fastify.listen(3000, '127.0.0.1', function (err) {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`);
+})
